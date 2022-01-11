959bcbc62014ce880dd5bbc202f9847b5ba2e458
nchern <nchern@inbox.ru>
2022-01-11 19:24:10 +0100

    pmail: initial commit

---
+++ pmail/go.mod
@@ -0,0 +1,5 @@
+module github.com/nchern/cli-tools/pmail
+
+go 1.15
+
+require github.com/DusanKasan/parsemail v1.2.0
---
+++ pmail/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/mail"
+	"os"
+
+	"github.com/DusanKasan/parsemail"
+)
+
+const (
+	cmdCC       = "cc"
+	cmdTo       = "to"
+	cmdBCC      = "bcc"
+	cmdFrom     = "from"
+	cmdSubject  = "subject"
+	cmdHTMLBody = "html"
+	cmdTextBody = "text"
+)
+
+type cmdFn func(parsemail.Email)
+
+var (
+	commands = map[string]cmdFn{
+		cmdSubject:  func(m parsemail.Email) { fmt.Println(m.Subject) },
+		cmdHTMLBody: func(m parsemail.Email) { fmt.Println(m.HTMLBody) },
+		cmdFrom:     func(m parsemail.Email) { printAddrs(m.From) },
+		cmdTo:       func(m parsemail.Email) { printAddrs(m.To) },
+		cmdCC:       func(m parsemail.Email) { printAddrs(m.Cc) },
+		cmdBCC:      func(m parsemail.Email) { printAddrs(m.Bcc) },
+		cmdTextBody: func(m parsemail.Email) { fmt.Println(m.TextBody) },
+	}
+)
+
+func printAddrs(addrs []*mail.Address) {
+	for i, addr := range addrs {
+		if i > 0 {
+			fmt.Print(",")
+		}
+		fmt.Print(addr)
+	}
+	fmt.Println()
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "Pmail - Parse Mail - is a tool to extract parts of email from a raw SMTP message.")
+	fmt.Fprintln(os.Stderr, "The message is expected on stdin.")
+	fmt.Fprintf(os.Stderr, "\nUsage:\n\n\t%s <command>\n", os.Args[0])
+	fmt.Fprintln(os.Stderr, "\nThe commands are:")
+
+	for cmd := range commands {
+		fmt.Fprintf(os.Stderr, "\t%s\n", cmd)
+	}
+
+	flag.PrintDefaults()
+}
+
+func init() {
+	flag.Usage = usage
+
+	flag.Parse()
+}
+
+func main() {
+	cmd := cmdTextBody
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
+	}
+
+	fn, found := commands[cmd]
+	if !found {
+		dieIf(fmt.Errorf("unknown command: %s", cmd))
+	}
+
+	email, err := parsemail.Parse(os.Stdin)
+	dieIf(err)
+
+	fn(email)
+}
+
+func dieIf(err error) {
+	if err != nil {
+		log.Fatal("fatal: ", err)
+	}
+}
