abeab386fac6415abbce791f30fe7e55794ad221
nchern <nchern@inbox.ru>
2025-05-03 13:40:01 +0200

    add calurl

---
+++ calurl/README.md
@@ -0,0 +1,4 @@
+# calurl
+
+`calurl` is a command-line utility that generates calendar event links for different provider.
+Currently Google Calendar, Outlook, or Apple Calendar are supported.
---
+++ calurl/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	flagTitle    = flag.String("t", "", "Event title (required)")
+	flagStart    = flag.String("s", "", "Start datetime (YYYY-MM-DDTHH:MM, required)")
+	flagEnd      = flag.String("e", "", "End datetime (YYYY-MM-DDTHH:MM, required)")
+	flagLocation = flag.String("l", "", "Location")
+	flagDesc     = flag.String("d", "", "Description")
+	flagTimezone = flag.String("z", "", "Timezone (default: system local)")
+	flagProvider = flag.String("p", "google", "Provider: google|outlook|apple (default: google)")
+)
+
+type event struct {
+	title    string
+	desc     string
+	location string
+
+	start time.Time
+	end   time.Time
+}
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	eventURL, err := mkUrl()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fatal: %s", err)
+		os.Exit(1)
+	}
+	fmt.Println(eventURL.String())
+}
+
+func parseAndValidate() (*event, error) {
+	if *flagTitle == "" || *flagStart == "" || *flagEnd == "" {
+		return nil, fmt.Errorf("-t, -s, and -e are required")
+	}
+	loc, err := parseTimezone(*flagTimezone)
+	if err != nil {
+		return nil, fmt.Errorf("bad timezone: %w", err)
+	}
+	startTime, err := parseTime(*flagStart, loc)
+	if err != nil {
+		return nil, fmt.Errorf("bad start time: %w", err)
+	}
+	endTime, err := parseTime(*flagEnd, loc)
+	if err != nil {
+		return nil, fmt.Errorf("bad end time: %w", err)
+	}
+	return &event{
+		title:    *flagTitle,
+		desc:     *flagDesc,
+		location: *flagLocation,
+
+		start: startTime,
+		end:   endTime,
+	}, nil
+}
+
+func mkUrl() (*url.URL, error) {
+	evt, err := parseAndValidate()
+	if err != nil {
+		return nil, err
+	}
+	switch strings.ToLower(*flagProvider) {
+	case "google":
+		return mkGoogleURL(evt)
+	case "outlook":
+		return mkOutlookURL(evt)
+	case "apple":
+		return mkAppleURL(evt)
+	}
+	return nil, fmt.Errorf("unknown provider: %s", *flagProvider)
+}
+
+func parseTimezone(tz string) (*time.Location, error) {
+	if tz == "" {
+		return time.Now().Location(), nil
+	}
+	return time.LoadLocation(tz)
+}
+
+func parseTime(datetime string, loc *time.Location) (time.Time, error) {
+	return time.ParseInLocation("2006-01-02T15:04", datetime, loc)
+}
+
+func formatICS(t time.Time) string {
+	return t.UTC().Format("20060102T150405Z")
+}
+
+// ------------------------- PROVIDER BUILDERS -------------------------
+
+func mkGoogleURL(evt *event) (*url.URL, error) {
+	u, err := url.Parse("https://calendar.google.com/calendar/render")
+	if err != nil {
+		return nil, err
+	}
+	params := url.Values{}
+	params.Set("action", "TEMPLATE")
+	params.Set("text", evt.title)
+	params.Set("dates", fmt.Sprintf("%s/%s", formatICS(evt.start), formatICS(evt.end)))
+	if evt.location != "" {
+		params.Set("location", evt.location)
+	}
+	if evt.desc != "" {
+		params.Set("details", evt.desc)
+	}
+	u.RawQuery = params.Encode()
+	return u, nil
+}
+
+func mkOutlookURL(evt *event) (*url.URL, error) {
+	u, err := url.Parse("https://outlook.live.com/owa/")
+	if err != nil {
+		return nil, err
+	}
+	params := url.Values{}
+	params.Set("path", "/calendar/action/compose")
+	params.Set("rru", "addevent")
+	params.Set("subject", evt.title)
+	params.Set("startdt", evt.start.Format(time.RFC3339))
+	params.Set("enddt", evt.end.Format(time.RFC3339))
+	if evt.location != "" {
+		params.Set("location", evt.location)
+	}
+	if evt.desc != "" {
+		params.Set("body", evt.desc)
+	}
+	u.RawQuery = params.Encode()
+	return u, nil
+}
+
+func mkAppleURL(evt *event) (*url.URL, error) {
+	u, err := url.Parse("webcal://example.com/event")
+	if err != nil {
+		return nil, err
+	}
+	params := url.Values{}
+	params.Set("title", evt.title)
+	params.Set("start", formatICS(evt.start))
+	params.Set("end", formatICS(evt.end))
+	if evt.location != "" {
+		params.Set("location", evt.location)
+	}
+	if evt.desc != "" {
+		params.Set("desc", evt.desc)
+	}
+	u.RawQuery = params.Encode()
+	return u, nil
+}
